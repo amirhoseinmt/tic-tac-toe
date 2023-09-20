0c396f93ee04c54d5c4157b721582ec0ade7d5dd
amirhosein mt <amirhoseinmirtalemi@gmail.com>
2023-09-20 19:33:51 +0330

    changing game digaram

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,11 @@
 import React, { useState } from 'react'
 import './App.css'
-import Board from './components/Board'
 
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(null))
-  const [xPlaying, setXPlaying] = useState(true)
-
-  const handleBoxClick = (boxIndex) => {
-    const updateBoard = board.map((value, index) => {
-      if( index === boxIndex) {
-        return xPlaying === true ? "X" : "O";
-      } else {
-        return value;
-      }
-    })
-
-    setBoard(updateBoard);
-    setXPlaying(!xPlaying);
-  }
 
   return (
     <div>
-      <Board board={board} onClick={handleBoxClick}/>
+      Hello
     </div>
   )
 }
