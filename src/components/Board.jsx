6c453b5f02dd0da5d35f688e88aadbeb53cf11b0
amirhosein mt <amirhoseinmirtalemi@gmail.com>
2023-09-20 18:20:47 +0330

    styling the board

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import Box from './Box'
+import './Board.css'
 
 const Board = ({ board, onClick }) => {
   return (
-    <div>   
+    <div className='board'>   
         {board.map((value, index) => {
             return <Box value={value} onClick={() => onClick(index)}/>            
         })}
