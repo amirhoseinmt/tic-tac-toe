57dbcdd3c8c1d11b35b906fd3c657b23536e9b63
amirhosein mt <amirhoseinmirtalemi@gmail.com>
2023-09-20 20:08:36 +0330

    board & tile CSS

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -4,15 +4,15 @@ import Strike from './Strike'
 
 const Board = () => {
   return (
-    <div>
-        <Tile />
-        <Tile />
-        <Tile />
-        <Tile />
-        <Tile />
-        <Tile />
-        <Tile />
-        <Tile />
+    <div className='board'>
+        <Tile className="right-border bottom-border"/>
+        <Tile className="right-border bottom-border"/>
+        <Tile className="bottom-border"/>
+        <Tile className="right-border bottom-border"/>
+        <Tile className="right-border bottom-border"/>
+        <Tile className="bottom-border"/>
+        <Tile className="right-border"/>
+        <Tile className="right-border"/>
         <Tile />
         <Strike />
     </div>
