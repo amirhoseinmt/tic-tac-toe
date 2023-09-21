84b802f1981619fdbee83eb4c76861170f9b7e5f
amirhosein mt <amirhoseinmirtalemi@gmail.com>
2023-09-21 11:47:48 +0330

    tiles & playerTurn states

--- src/components/TicTacToe.jsx
+++ src/components/TicTacToe.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Board from './Board'
 
+const PLAYER_X = "X";
+const PLAYER_O = "O";
+
 const TicTacToe = () => {
+  const [tiles, setTiles] = useState(Array(9).fill(null));
+  const [playerTurn, setPlayerTurn] = useState(PLAYER_X);
+
   return (
     <div>
         <h1>TicTacToe</h1>
-        <Board />
+        <Board tiles={tiles}/>
     </div>
   )
 }
